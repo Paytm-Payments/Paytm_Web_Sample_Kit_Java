c37088aa092def0467d775e79aad46e1ec7ea6fb
Prabhat Mishra <35480210+Prabhat1Mishra@users.noreply.github.com>
2018-03-06 12:29:22 +0530

    Upgraded to PGP...

--- Sample Kit/paytm_java/src/paytm_java/PaytmConstants.java
+++ Sample Kit/paytm_java/src/paytm_java/PaytmConstants.java
@@ -5,6 +5,6 @@ public class PaytmConstants {
   public final static String INDUSTRY_TYPE_ID="Retail";
   public final static String CHANNEL_ID="WEB";
   public final static String WEBSITE="worldpressplg";
-  public final static String PAYTM_URL="https://pguat.paytm.com/oltp-web/processTransaction"; 
+  public final static String PAYTM_URL="https://securegw-stage.paytm.in/theia/processTransaction"; 
   
 }
